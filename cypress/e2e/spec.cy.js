f0c6c77db19e85cba3291e63c62eb3d08dec08a7
mgdev08 <vaval.mg.08@gmail.com>
2024-04-14 09:48:17 +0200

    Add Cypress command visitUrl

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+
+  it('connects to website', () => {
     cy.visit('index.html')
-  })
-})
+  });
+
+  it('goes to topics-detail', () => {
+    cy.visitUrl('index.html')
+    cy.xpath("//*[@id='top']/main/section[2]/div/div/div[1]/div/a").click()
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('visitUrl', (url) => {
+    cy.visit(url)
+});
